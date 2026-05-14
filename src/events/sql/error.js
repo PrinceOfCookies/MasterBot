30f76f7dfc5e709e10cd6ed647098b8b39e6eb76
PrinceOfCookies <iuploadgmod@gmail.com>
2026-05-14 10:30:43 -0500

    Moved these from strw here, since *any* bot using sql is gonna use these

---
+++ src/events/sql/error.js
@@ -0,0 +1,20 @@
+const { red } = require("chalk");
+
+module.exports = {
+  name: "error",
+  async execute(err) {
+    const details = [
+      `An error occured with the database connection:`,
+      err?.code ? `code: ${err.code}` : null,
+      err?.sqlMessage ? `sqlMessage: ${err.sqlMessage}` : null,
+      err?.sql ? `sql: ${err.sql}` : null,
+      err?.params ? `params: ${JSON.stringify(err.params)}` : null,
+      err?.stack ? err.stack : String(err),
+    ]
+      .filter(Boolean)
+      .join("\n");
+
+    console.log(red(details));
+  },
+  color: "#00FF00",
+};

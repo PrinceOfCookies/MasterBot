3bd9ab829b1c8cc098504b4ce234d8b773db6161
PrinceOfCookies <iuploadgmod@gmail.com>
2026-05-14 11:09:02 -0500

    Update ready prints

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,6 +3,23 @@ module.exports = {
 	once: true,
 
 	execute(client) {
-		console.log(`[${client.botName}] Ready as ${client.user.tag}`);
+		console.log(
+			await client.color(
+			  "#2afeb7",
+			  `[Client Status]: Logged in as ${client.user.tag}`
+			)
+		  );
+
+		  const guilds = client.guilds.cache.size;
+    
+		  // Get the users in all guilds (not counting bots)
+		  const users = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
+		  
+		  console.log(
+			await client.color(
+			  "#2afeb7",
+			  `[Client Status]: Serving ${users} users in ${guilds} guild(s).`
+			)
+		  );
 	}
 };
